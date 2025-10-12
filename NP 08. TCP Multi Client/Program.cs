13069bdf0a0bec7630b96620ff8a0de4f5075bb2
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2025-10-12 11:45:11 +0400

    MultiChat

---
+++ NP 08. TCP Multi Client/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+var client = new TcpClient();
+var ip = IPAddress.Parse("10.2.24.1");
+var port = 27001;
+
+try
+{
+	client.Connect(ip, port);
+    Console.WriteLine("Connected to server");
+	var stream = client.GetStream();
+	var br = new BinaryReader(stream);
+	var bw = new BinaryWriter(stream);
+	var sendingTask = Task.Run(()=>
+	{
+		while (true)
+		{
+			var message = Console.ReadLine();
+			if (string.IsNullOrEmpty(message)) continue;
+			try
+			{
+				bw.Write(message);
+				bw.Flush();
+			}
+			catch (Exception ex)
+			{
+                Console.WriteLine($"Sending Error: {ex.Message}");
+				break;
+			}
+		}
+	});
+
+	var receivingTask = Task.Run(() =>
+	{
+		while (true)
+		{
+			try
+			{
+				var received = br.ReadString();
+                Console.WriteLine($"{received}");
+			}
+			catch (Exception ex)
+			{
+                Console.WriteLine($"Receiving error: {ex.Message}");
+				break;
+			}
+		}
+	});
+
+	await Task.WhenAny(sendingTask, receivingTask);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Connection Error: {ex.Message}");
+}
+finally
+{
+	client.Close();
+}
+
+
---
+++ NP 08. TCP Multi Listener/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Concurrent;
+using System.Net;
+using System.Net.Sockets;
+
+TcpListener listener = default;
+
+ConcurrentBag<TcpClient> clients = new();
+
+var ip = IPAddress.Parse("10.2.24.1");
+var port = 27001;
+
+listener = new TcpListener(ip, port);
+
+listener.Start();
+
+Console.WriteLine($"Listening on {listener.LocalEndpoint}");
+
+
+
+_ = Task.Run(()=>
+{
+	while (true)
+	{
+		var message = Console.ReadLine();
+		foreach (var client in clients)
+		{
+			try
+			{
+				var stream = client.GetStream();
+				var bw = new BinaryWriter(stream);
+				bw.Write(message!);
+				bw.Flush();
+			}
+			catch (Exception)
+			{
+                Console.WriteLine($"Client send message failed. Skipping...");
+			}
+		}
+	}
+});
+
+while (true)
+{
+	var client = listener.AcceptTcpClient();
+	clients.Add(client);
+
+    Console.WriteLine($"{client.Client.RemoteEndPoint} connected");
+
+	_ = Task.Run(() =>
+	{
+		try
+		{
+			var stream = client.GetStream();
+			var br = new BinaryReader(stream);
+			var bw = new BinaryWriter(stream);
+			while (true)
+			{
+				var message = br.ReadString();
+				foreach (var cl in clients)
+				{
+					if (client.Client.RemoteEndPoint != cl.Client.RemoteEndPoint)
+					{
+						var clientBw = new BinaryWriter(cl.GetStream());
+						clientBw.Write($"{cl.Client.LocalEndPoint}: {message}");
+					}
+				}
+                Console.WriteLine($"Client: {client.Client.RemoteEndPoint}: {message}");
+			}
+		}
+		catch (Exception)
+		{
+            Console.WriteLine($"Client {client.Client.RemoteEndPoint} disconnected");
+		}
+	});
+}
+
